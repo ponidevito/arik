7b2a48c8df96d87b61da23085ce1df2c4cd7a72a
unknown <olegmama@gmail.com>
2025-07-05 11:20:10 +0300

    added section cta and talk

--- src/js/main.js
+++ src/js/main.js
@@ -25,29 +25,99 @@ function burgerMenu() {
 }
 burgerMenu();
 
+// document.addEventListener("DOMContentLoaded", function () {
+//   const submenuLink = document.querySelector(".menu__item-link.sub");
+//   const submenu = submenuLink.nextElementSibling;
 
-document.addEventListener('DOMContentLoaded', function () {
-  const submenuLink = document.querySelector('.menu__item-link.sub');
+//   submenuLink.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     e.stopPropagation();
+
+//     // Перемикання submenu
+//     submenu.classList.toggle("active");
+//     submenuLink.classList.toggle("active"); // додаємо клас до лінку для анімації
+//   });
+
+//   // Закриває submenu при кліку поза меню
+//   document.addEventListener("click", function (e) {
+//     if (
+//       !e.target.closest(".menu__item-link.sub") &&
+//       !e.target.closest(".submenu")
+//     ) {
+//       submenu.classList.remove("active");
+//       submenuLink.classList.remove("active"); // знімаємо обертання
+//     }
+//   });
+// });
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   const content = document.querySelector(".talk__content");
+//   const item = content.querySelector(".talk__item");
+
+//   // Клонуємо item і додаємо його для безшовного ефекту
+//   const clone = item.cloneNode(true);
+//   content.appendChild(clone);
+
+//   // Розрахунок повної ширини (одного item)
+//   const itemWidth = item.offsetWidth;
+
+//   // Запускаємо анімацію
+//   gsap.to(content, {
+//     x: `-=${itemWidth}`,
+//     duration: 30,
+//     ease: "none",
+//     repeat: -1,
+//     modifiers: {
+//       x: gsap.utils.unitize(x => parseFloat(x) % itemWidth),
+//     },
+//   });
+// });
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () { 
+  // submenu
+  const submenuLink = document.querySelector(".menu__item-link.sub");
   const submenu = submenuLink.nextElementSibling;
 
-  submenuLink.addEventListener('click', function (e) {
+  submenuLink.addEventListener("click", function (e) {
     e.preventDefault();
     e.stopPropagation();
 
     // Перемикання submenu
-    submenu.classList.toggle('active');
-    submenuLink.classList.toggle('active'); // додаємо клас до лінку для анімації
+    submenu.classList.toggle("active");
+    submenuLink.classList.toggle("active"); // додаємо клас до лінку для анімації
   });
 
-  // Закриває submenu при кліку поза меню
-  document.addEventListener('click', function (e) {
-    if (!e.target.closest('.menu__item-link.sub') && !e.target.closest('.submenu')) {
-      submenu.classList.remove('active');
-      submenuLink.classList.remove('active'); // знімаємо обертання
+  document.addEventListener("click", function (e) {
+    if (
+      !e.target.closest(".menu__item-link.sub") &&
+      !e.target.closest(".submenu")
+    ) {
+      submenu.classList.remove("active");
+      submenuLink.classList.remove("active"); // знімаємо обертання
     }
   });
+
+
+  // infiniti text
+  const content = document.querySelector(".talk__content");
+  const item = content.querySelector(".talk__item");
+
+  const clone = item.cloneNode(true);
+  content.appendChild(clone);
+
+  const itemWidth = item.offsetWidth;
+
+  gsap.to(content, {
+    x: `-=${itemWidth}`,
+    duration: 30,
+    ease: "none",
+    repeat: -1,
+    modifiers: {
+      x: gsap.utils.unitize(x => parseFloat(x) % itemWidth),
+    },
+  });
 });
-
-
-
-
